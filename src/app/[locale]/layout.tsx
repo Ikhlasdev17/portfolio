97e7d68664ac0b974e2bac7202fa46cfbc136047
Ikhlasdev17 <iklasaralbaev@gmail.com>
2024-06-02 04:35:43 +0500

    yandex metrika added

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import Header from '@/components/layout/header'
 import { Providers } from '@/providers'
 import TranslationsProvider from '@/providers/translations-provider'
+import Script from 'next/script'
 import initTranslations from '../i18n'
 import './globals.css'
 
@@ -58,6 +59,25 @@ export default async function RootLayout({
 
 	return (
 		<html lang='en' className='!scroll-smooth dark' suppressHydrationWarning>
+			<Script>
+				{`<!-- Yandex.Metrika counter -->
+				<script type="text/javascript" >
+					(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+					m[i].l=1*new Date();
+					for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+					k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+					(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+					ym(97459122, "init", {
+								clickmap:true,
+								trackLinks:true,
+								accurateTrackBounce:true,
+								webvisor:true
+					});
+				</script>
+				<noscript><div><img src="https://mc.yandex.ru/watch/97459122" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+				<!-- /Yandex.Metrika counter -->`}
+			</Script>
 			<body className={`${inter.className} bg-gray text-gray-600 antialiased`}>
 				<main className='flex min-h-screen w-full flex-col'>
 					<Providers>
