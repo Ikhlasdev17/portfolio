79e7605d0bd9ed2e0344a1d618d6a78eeeb7c48c
Ikhlasdev17 <iklasaralbaev@gmail.com>
2024-06-02 04:37:42 +0500

    yandex metrika bug fixed

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -59,9 +59,8 @@ export default async function RootLayout({
 
 	return (
 		<html lang='en' className='!scroll-smooth dark' suppressHydrationWarning>
-			<Script>
+			<Script id='yandex-metrika'>
 				{`<!-- Yandex.Metrika counter -->
-				<script type="text/javascript" >
 					(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
 					m[i].l=1*new Date();
 					for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
@@ -74,7 +73,6 @@ export default async function RootLayout({
 								accurateTrackBounce:true,
 								webvisor:true
 					});
-				</script>
 				<noscript><div><img src="https://mc.yandex.ru/watch/97459122" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 				<!-- /Yandex.Metrika counter -->`}
 			</Script>
